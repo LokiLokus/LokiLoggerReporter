6e88d28fc982ca9d5a551908591a02b44ae6d26e
lokilokus <sailerq@in.tum.de>
2019-05-29 10:12:43 +0200

    Fixed Frontend

--- wwwroot/components/dashboard-card.js
+++ wwwroot/components/dashboard-card.js
@@ -1,9 +1,11 @@
 Vue.component('dashboard-card', {
-    props:['color','count'],
+    
+    props:['color','count','type'],
+    
     template: '<div class="col-xl-3 col-sm-6 mb-3">\n' +
         '        <div :class="color" class="card text-white o-hidden h-100">\n' +
         '            <div class="card-body">\n' +
-        '                <div class="mr-5">{{count}} New Information</div>\n' +
+        '                <div class="mr-5">{{count}} New </div>\n' +
         '            </div>\n' +
         '            <a class="card-footer text-white clearfix small z-1" href="#">\n' +
         '                <span class="float-left">View Details</span>\n' +
