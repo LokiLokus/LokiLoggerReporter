c34240dc745a905da6030831fb9b347c8b4411d3
lokilokus <sailerq@in.tum.de>
2019-07-23 23:56:50 +0200

    Added Bootstrap Vue

---
+++ wwwroot/js/table.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+    data:function(){
+        return{
+            data:[],
+            show: true
+        }
+    },
+    methods: {
+        getData: function () {
+            axios.get('/api/Source/All/')
+                .then(x => {
+                    this.data = x.data;
+                }).catch(x => {
+                if(x.response){
+                    this.errors = x.response.data;
+                }else{
+                    console.log(x)
+                    alert("Ein Fehler ist aufgetreten");
+                }
+            });
+        }
+    },
+    mounted: function () {
+        this.getData()
+    },
+    computed: {
+        isEmpty: function () {
+            return isEmpty(this.selData)
+        },
+
+    },
+    filters: {
+    },
+    el:"#tableAnalyze"
+});
