6f459270df8f00a9e358c39daa9e846a7782d2fb
lokilokus <sailerq@in.tum.de>
2019-07-27 23:19:51 +0200

    Extended Table View

--- wwwroot/js/table.js
+++ wwwroot/js/table.js
@@ -5,16 +5,16 @@ var app = new Vue({
             show: true,
             totalRows: 1,
             currentPage: 1,
-            perPage: 5,
+            perPage: 30,
             pageOptions: [5, 10, 15],
             sortBy: null,
             sortDesc: false,
             sortDirection: 'asc',
             fields: [
-                { key: 'LogLevel', label: 'Lvl', sortable: true},
-                { key: 'LogTyp', label: 'Typ', sortable: true },
+                { key: 'LogLevel', label: 'Lvl', sortable: true, class: 'text-center'},
+                { key: 'LogTyp', label: 'Typ', sortable: true, class: 'text-center'},
                 { key: 'Time', label: 'Time',sortable:true },
-                { key: 'actions', label: 'Actions' }
+                { key: 'Message', label: 'Nachricht',sortable:true }
             ],
             filter: null,
         }
@@ -24,6 +24,7 @@ var app = new Vue({
             axios.get('/api/Logging/GetLogBySource/RRS/0-100')
                 .then(x => {
                     this.data = x.data;
+                    this.totalRows = this.data.length
                 }).catch(x => {
                 if(x.response){
                     this.errors = x.response.data;
@@ -32,6 +33,10 @@ var app = new Vue({
                     alert("Ein Fehler ist aufgetreten");
                 }
             });
+        },
+        expandAdditionalInfo: function(row){
+            console.log(row)
+            row._showDetails = !row._showDetails;
         },
       onFiltered(filteredItems) {
         // Trigger pagination to update the number of buttons/pages due to filtering
@@ -50,16 +55,14 @@ var app = new Vue({
         }
     },
     mounted() {
-        // Set the initial number of items
         this.getData();
-        this.totalRows = this.data.length
     },
     filters: {
         date: function (value) {
             if (!value) return '';
             var t = new Date(value);
             var dd = t.getDate();
-            var mm = t.getMonth()+1; //January is 0!
+            var mm = t.getMonth()+1;
             var yyyy = t.getFullYear();
             var hh = t.getHours();
             var mMin = t.getMinutes();
@@ -72,6 +75,33 @@ var app = new Vue({
                 mm='0'+mm
             }
             return yyyy + '.' + mm + '.' + dd + ' - ' + hh + ':' + mMin + ':' + ss ;
+        },
+        message:function (value) {
+            if (!value || !value.Message) return '';
+            var datasets = [];
+            var re = / {[\s\S]*?}/;
+            var message = value.Message;
+            var result = "";
+            
+            try{
+                datasets = JSON.parse(value.Data);
+            }catch(err){}
+            
+            if(Array.isArray(datasets)){
+                for (let i = 0; i < datasets.length; i++) {
+                    match = re.exec(message);
+                    if(match != null){
+                        var tmp = message.substr(match.index + match[0].length);
+                        result += message.substr(0, match.index) + ' ' + JSON.stringify(datasets[i]);
+                        
+                        message = tmp;
+                    }
+                }
+                
+            }
+            value.Message = result;
+            
+            return result.substr(0,100) + "...";
         }
     },
     el:"#tableAnalyze"
