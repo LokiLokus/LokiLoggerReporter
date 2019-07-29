da64e54c96fce24181f7204cac79057e6db609bb
Paul Müller <paulus_mllerus@yahoo.de>
2019-07-29 14:15:53 +0000

    Update Extensions/Time.cs

--- Extensions/Time.cs
+++ Extensions/Time.cs
@@ -1,24 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using lokiloggerreporter.Extensions;
+using lokiloggerreporter.Models;
+using lokiloggerreporter.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
 
 using System.IO;
 using System.Collections;
-using System.Linq;
 namespace lokiloggerreporter.Extensions {
 	public static class Time {
 
 		public static DateTime Now => DateTime.Now;
 
 		public static IEnumerable<TSource> DistinctBy<TSource, TKey>
-    (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-{
-    HashSet<TKey> seenKeys = new HashSet<TKey>();
-    foreach (TSource element in source)
-    {
-        if (seenKeys.Add(keySelector(element)))
+        (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
         {
-            yield return element;
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
         }
-    }
-}
 	}
 }
