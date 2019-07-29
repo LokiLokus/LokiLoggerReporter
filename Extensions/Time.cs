0313dc785ff6644490d904fc64f8ab6b34d7af8a
Paul Müller <paulus_mllerus@yahoo.de>
2019-07-29 14:12:23 +0000

    Added DistinctBy

--- Extensions/Time.cs
+++ Extensions/Time.cs
@@ -4,5 +4,18 @@ namespace lokiloggerreporter.Extensions {
 	public static class Time {
 
 		public static DateTime Now => DateTime.Now;
+
+		public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+    (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+{
+    HashSet<TKey> seenKeys = new HashSet<TKey>();
+    foreach (TSource element in source)
+    {
+        if (seenKeys.Add(keySelector(element)))
+        {
+            yield return element;
+        }
+    }
+}
 	}
 }
