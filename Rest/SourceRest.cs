9c4c9eb14056cfc5da9db0aedeca44712b3b5e4d
Paul Müller <paulus_mllerus@yahoo.de>
2019-07-29 14:22:53 +0000

    Update Rest/SourceRest.cs

--- Rest/SourceRest.cs
+++ Rest/SourceRest.cs
@@ -37,7 +37,7 @@ namespace lokiloggerreporter.Rest {
 				{
 					Source = x.Key,
 					Count = x.Sum(f => 1),
-					AllCount = x.Sum(f => 1),
+					AllCount = ctx.Where(f => f.Name == x.Name),
 					Level = LogLevelExtension.Levels().Select(l =>
 						new
 						{
