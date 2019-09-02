57c69ff232fe3690b37059fdbe465115696a5740
Loki <sailerq@in.tum.de>
2019-09-03 00:02:03 +0200

    Added Buce FraohQL Stuff

---
+++ Extensions/MyDefaultGraphQLExecuter.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading;
+using GraphQL;
+using GraphQL.Execution;
+using GraphQL.Server;
+using GraphQL.Server.Internal;
+using GraphQL.Types;
+using GraphQL.Validation;
+using Microsoft.Extensions.Options;
+
+namespace lokiloggerreporter.Extensions {
+	public class MyDefaultGraphQLExecuter<TSchema> : DefaultGraphQLExecuter<TSchema>
+		where TSchema : ISchema
+	{
+
+		public MyDefaultGraphQLExecuter(TSchema schema, IDocumentExecuter documentExecuter, IOptions<GraphQLOptions> options, IEnumerable<IDocumentExecutionListener> listeners, IEnumerable<IValidationRule> validationRules) 
+			: base(schema, documentExecuter, options, listeners, validationRules)
+		{
+		}
+
+		protected override ExecutionOptions GetOptions(string operationName, string query, Inputs variables, object context, CancellationToken cancellationToken)
+		{
+			var options = base.GetOptions(operationName, query, variables, context, cancellationToken);
+			options.FieldNameConverter = Schema.FieldNameConverter;
+			return options;
+		} 
+	}
+}
