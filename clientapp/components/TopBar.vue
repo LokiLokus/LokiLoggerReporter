8e05a91051ee8c51824ba749817e4f370c5f6b5d
LokiLokus <quirin.sailer@sailersoftware.de>
2019-11-04 22:41:12 +0100

    Clientapp

--- clientapp/components/TopBar.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <nav style="height:1px;" class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-    <!-- Sidebar Toggle (Topbar) .-->
-    <div ></div>
-  </nav>
-</template>
