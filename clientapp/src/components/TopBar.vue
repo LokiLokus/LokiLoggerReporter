c8bd2cea1271b2f19f4a82ef1ca637d7234038d6
LokiLokus <quirin.sailer@sailersoftware.de>
2019-11-04 22:42:28 +0100

    ClA

---
+++ clientapp/src/components/TopBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <nav style="height:1px;" class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    <!-- Sidebar Toggle (Topbar) .-->
+    <div ></div>
+  </nav>
+</template>
