5ee49f174b1e8e9397564a11e5cc43b15e9623cc
LokiLokus <quirin.sailer@sailersoftware.de>
2019-11-23 01:58:02 +0100

    Removed Remove

--- LokiLoggerReporter/Services/Implementation/RestAnalyzeService.cs
+++ LokiLoggerReporter/Services/Implementation/RestAnalyzeService.cs
@@ -100,19 +100,19 @@ namespace lokiloggerreporter.Services.Implementation
                 
                 if (model.FromTime != null)
                 {
-                    /*
-                    Dictionary<DateTime,List<WebRequest>> preorderdCache = new Dictionary<DateTime, List<WebRequest>>();
-                    int splitSize = (int)(model.Resolution / 10);
-                    foreach (var dateTime in fromTimes.SplitList(splitSize))
-                    {
-                        preorderdCache.Add(dateTime.FirstOrDefault(),endPointUsage.WebRequests.Where(x => x.Start >= dateTime.FirstOrDefault() && x.Start <= dateTime.Last()).ToList());
-                    }*/
+                    
                     foreach (var x in fromTimes)
                     {
                         var tmpData = CalculateLeaveTimeSteps(x, analyzeSpan,endPointUsage.WebRequests);
                         endPointUsage.TimeSlots.Add(tmpData);
                     }
-                    
+                    /*
+                                        Dictionary<DateTime,List<WebRequest>> preorderdCache = new Dictionary<DateTime, List<WebRequest>>();
+                                        int splitSize = (int)(model.Resolution / 10);
+                                        foreach (var dateTime in fromTimes.SplitList(splitSize))
+                                        {
+                                            preorderdCache.Add(dateTime.FirstOrDefault(),endPointUsage.WebRequests.Where(x => x.Start >= dateTime.FirstOrDefault() && x.Start <= dateTime.Last()).ToList());
+                                        }
                     var first = endPointUsage.TimeSlots.First();
                     
                     var toRemove =  new List<RequestAnalyzeModel>();
@@ -126,7 +126,7 @@ namespace lokiloggerreporter.Services.Implementation
                         }
                         first = endPointUsage.TimeSlots[i];
                     }
-                    endPointUsage.TimeSlots.RemoveAll(x => toRemove.Contains(x));
+                    endPointUsage.TimeSlots.RemoveAll(x => toRemove.Contains(x));*/
                 }
 
                 endPointUsage.Processed = true;
@@ -156,7 +156,7 @@ namespace lokiloggerreporter.Services.Implementation
                         foreach (var dateTime in fromTimes.SplitList(splitSize))
                         {
                             preorderdCache.Add(dateTime.FirstOrDefault(),requests.Where(x => x.FromTime >= dateTime.FirstOrDefault() && x.FromTime <= dateTime.Last()).ToList());
-                        }*/
+                        }
                         
                         
                         var first = tmp.TimeSlots.First();
@@ -174,7 +174,7 @@ namespace lokiloggerreporter.Services.Implementation
 
                             first = tmp.TimeSlots[i];
                         }
-                        tmp.TimeSlots.RemoveAll(x => toRemove.Contains(x));
+                        tmp.TimeSlots.RemoveAll(x => toRemove.Contains(x));*/
                     }
                 });
             }
